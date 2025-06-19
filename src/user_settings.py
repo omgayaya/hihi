b88e65c1f040888349a307cb7800a6fc4e1217da
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-06-19 19:26:20 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -5,6 +5,7 @@
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
     "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
+    "https://t.me/s/v2ray_vpn_ir",
     "https://t.me/s/v2rayfree",
     "https://t.me/s/FreeV2rays",
     "https://t.me/s/v2ray_free_conf",
