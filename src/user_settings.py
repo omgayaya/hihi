0936b7208381445455712237d62f041153984982
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-06-21 01:17:43 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,7 +4,7 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
-    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/splitted/mixed",
+    #"https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/splitted/mixed",
     "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
     "https://t.me/s/v2ray_vpn_ir",
     "https://t.me/s/v2rayfree",
