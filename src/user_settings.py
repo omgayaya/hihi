9fa83213391ce51567715ca14079c9a4e53964fb
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-07-08 20:50:46 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -24,6 +24,7 @@ SOURCE_URLS = [
     # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/shadowsocks",
     # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/hysteria", 
     "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
+    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/lite/subscriptions/xray/normal/mix",
     # Add more URLs here if you want to include additional sources.
 ]
 
